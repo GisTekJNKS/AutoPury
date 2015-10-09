cd2886e7522788b30077b671e3b18a14355eaa70
Makhankov <daupsarim@gmail.com>
2015-10-09 10:19:26 +0300

    segfsag

--- pury/src/test/java/TestSuccessfulAuthorization.java
+++ pury/src/test/java/TestSuccessfulAuthorization.java
@@ -1,10 +1,13 @@
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 /**
  * Created by John on 07.10.2015.
@@ -16,7 +19,9 @@ public class TestSuccessfulAuthorization {
     public void ready(){
         Configuration.browser = System.getProperty("browser", "firefox");
         open("http://purityIT:HardPassword@purity_ws.andersenlab.com");
-        $("#show-login-form-btn").click();
+//        $("#show-login-form-btn").click();
+        getWebDriver().findElement(By.id("show-login-form-btn")).click();
+
     }
 
 
